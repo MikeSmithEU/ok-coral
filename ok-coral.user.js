47e59f89d2959d921ec5e0fd3fcb4cdce4a086ec
MikeSmithEU <2065591+MikeSmithEU@users.noreply.github.com>
2021-09-08 11:10:33 +0200

    update version

--- ok-coral.user.js
+++ ok-coral.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OK Coral
 // @namespace    https://mikesmith.eu
-// @version      0.6
+// @version      0.7
 // @description  Make Coral comments OK again!
 // @author       MikeSmithEU
 // @match        https://*.coral.coralproject.net/*
